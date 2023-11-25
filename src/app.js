b528fa175b0161ede2a44ba7152e7e7a2f35edec
Alivafo001 <moneycapital001@gmail.com>
2023-11-25 12:10:17 +0300

    Errors fixed

--- src/app.js
+++ src/app.js
@@ -2,8 +2,11 @@ let btn = document.querySelector('#btn');
 let nav = document.querySelector('#nav');
 let x = document.querySelector('#x')
 let body = document.querySelector('body')
+let card = document.querySelector('#card')
+let more = document.querySelector('#more')
+let zero = document.querySelector('#zero')
 
-
+// header navigation bar start
 btn.addEventListener('click' , function(){
     nav.classList.remove("left-full")
     nav.classList.add("left-0")
@@ -13,3 +16,4 @@ x.addEventListener('click' , function(){
     nav.classList.add("left-full")
     nav.classList.remove("left-0")
 })
+// header navigation bar end
